107c3e34c6782c0f18840ea8ff01bd835e14fcdd
Vinojan Veerapathrathasan <vinojan02abhimanyu@gmail.com>
2022-07-07 00:31:15 +0530

    Added new export methods

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
-function Greet() {
-    return <h1>Hello Vinojan</h1>
-}
+// function Greet() {
+//     return <h1>Hello Vinojan</h1>
+// }
+
+const Greet = () => <h1>Hello Vinojan Abhimanyu</h1> 
 
 export default Greet
